1354991607106606b98e5ade981bd98b67b9f368
nikp00 <nik.princic@gmail.com>
2021-05-15 20:45:37 +0200

    Fixed timestamp to 1h intervals

--- utils/get_prices.py
+++ utils/get_prices.py
@@ -8,6 +8,16 @@ import time
 import datetime
 
 
+def convert_time(x, micros=False):
+    if micros:
+        x = datetime.datetime.fromtimestamp(x / 1000.0)
+    else:
+        x = datetime.datetime.fromtimestamp(x)
+    x = x.replace(minute=0, second=0, microsecond=0)
+    x = int(datetime.datetime.timestamp(x))
+    return x
+
+
 def get_data(symbol, symbol_id, start, end, path):
 
     r = requests.get(
@@ -27,15 +37,56 @@ def get_data(symbol, symbol_id, start, end, path):
     data = r.json()
 
     df = pd.DataFrame(columns=["date", "price"], data=data["prices"])
+    df["date"] = df["date"].apply(lambda x: convert_time(x, micros=True))
     df.to_csv(path + "/" + symbol + "_price.csv", index=False)
 
     df = pd.DataFrame(columns=["date", "market_cap"], data=data["market_caps"])
+    df["date"] = df["date"].apply(lambda x: convert_time(x, micros=True))
     df.to_csv(path + "/" + symbol + "_market_cap.csv", index=False)
 
     df = pd.DataFrame(columns=["date", "volume"], data=data["total_volumes"])
+    df["date"] = df["date"].apply(lambda x: convert_time(x, micros=True))
     df.to_csv(path + "/" + symbol + "_volume.csv", index=False)
 
     print(f"{symbol} saved.")
+
+
+def find_min_max_date(path):
+    min_date = datetime.datetime.utcnow().timestamp()
+    max_date = 0
+    path = os.path.join(path, "posts")
+    print(path)
+    for source in os.listdir(path):
+        if not os.path.isdir(os.path.join(path, source)):
+            continue
+
+        for e in os.listdir(os.path.join(path, source)):
+            if os.path.isdir(os.path.join(path, source, e)):
+                for x in os.listdir(os.path.join(path, source, e)):
+                    if x.endswith(".csv"):
+                        df = pd.read_csv(os.path.join(path, source, e, x))
+                        c_min = df["date"].min()
+                        c_max = df["date"].max()
+            elif e.endswith(".csv"):
+                df = pd.read_csv(os.path.join(path, source, e))
+                c_min = df["date"].min()
+                c_max = df["date"].max()
+
+            if c_min < min_date:
+                min_date = c_min
+            if c_max > max_date:
+                max_date = c_max
+
+    print(min_date, max_date)
+    min_date = datetime.datetime.fromtimestamp(min_date)
+    min_date = min_date.replace(minute=0, second=0, microsecond=0)
+    min_date = int(datetime.datetime.timestamp(min_date))
+
+    max_date = datetime.datetime.fromtimestamp(max_date)
+    max_date = max_date.replace(minute=0, second=0, microsecond=0)
+    max_date = int(datetime.datetime.timestamp(max_date))
+
+    return min_date, max_date
 
 
 if __name__ == "__main__":
@@ -46,29 +97,7 @@ if __name__ == "__main__":
     limit = tracked_coins["count"].quantile(0.75)
     tracked_coins = tracked_coins[tracked_coins["count"] >= limit]
 
-    min_date = datetime.datetime.utcnow().timestamp()
-    max_date = 0
-
-    for source in os.listdir(path):
-        if not os.path.isdir(path + source) or source == "price_data":
-            continue
-
-        for e in os.listdir(path + source):
-            if os.path.isdir(path + source + "/" + e):
-                for x in os.listdir(path + source + "/" + e):
-                    if x.endswith(".csv"):
-                        df = pd.read_csv(path + source + "/" + e + "/" + x)
-                        c_min = df["date"].min()
-                        c_max = df["date"].max()
-            elif e.endswith(".csv"):
-                df = pd.read_csv(path + source + "/" + e)
-                c_min = df["date"].min()
-                c_max = df["date"].max()
-
-            if c_min < min_date:
-                min_date = c_min
-            if c_max > max_date:
-                max_date = c_max
+    min_date, max_date = find_min_max_date(path)
 
     if not os.path.exists(path + "price_data"):
         os.mkdir(path + "price_data")
