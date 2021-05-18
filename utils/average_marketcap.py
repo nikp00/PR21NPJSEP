c895792054a99679375577ed0bcf2b2800e09ab7
Jaka <jakaskerjanc@gmail.com>
2021-05-18 16:29:00 +0200

    report pics

--- utils/average_marketcap.py
+++ utils/average_marketcap.py
@@ -1,9 +1,35 @@
-import os
 import pandas as pd
 import numpy as np
-
 import matplotlib.pyplot as plt
-import numpy as np
+from matplotlib import rcParams
+import datetime
+from collections import defaultdict
+
+import os
+
+BASE_PATH = os.path.abspath(os.path.curdir)
+PRICE_DATA_DIR = os.path.join(BASE_PATH, "data/price_data/")
+MENTIONS_COUNT_DIR = os.path.join(BASE_PATH, "data/date_count")
+
+COLOR_MAIN = "#363A45"
+COLOR_MA_5 = "#FF9800"
+COLOR_MA_8 = "#E040FB"
+COLOR_MA_13 = "#9C27B0"
+COLOR_VOLUME = "#B2B5BE"
+
+MIN_DATE = "2021-02-01 00:00:00"
+MAX_DATE = "2021-04-05 03:00:00"
+
+TITLE_SIZE = 20
+AXES_LABELS_SIZE = 15
+
+DATE_RANGE = pd.date_range(
+    start=MIN_DATE,
+    end=MAX_DATE,
+    freq="2D",
+)
+
+plt.style.use("seaborn")
 
 if __name__ == "__main__":
     path = os.path.dirname(__file__)
@@ -44,16 +70,21 @@ if __name__ == "__main__":
     width = 0.35  # the width of the bars
 
     fig, ax = plt.subplots()
-    rects1 = ax.bar(x - width/2, sortedDf["mention_percentage"][1:20], width, label='Obembe')
-    rects2 = ax.bar(x + width/2, sortedDf["market_cap_percentage"][1:20], width, label='Tržna kapitalizacija')
+    rects1 = ax.bar(x - width/2, sortedDf["mention_percentage"][1:20], width, label='Mentions')
+    rects2 = ax.bar(x + width/2, sortedDf["market_cap_percentage"][1:20], width, label='Market cap')
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_ylabel('%')
-    ax.set_title('Odstotek obemb proti odstotku tržne kapitalizacije (razporejeno po največji razliki)')
+    ax.set_title('Percentage of mentions in comparison to market capitalisation')
     ax.set_xticks(x)
     ax.set_xticklabels(labels,rotation=90)
     ax.legend()
 
     fig.tight_layout()
 
-    plt.show()
+    plt.savefig(
+        os.path.join(
+            BASE_PATH, "visualization/", "market_cap_vs_mentions.png"
+        )
+    )
+    plt.close(fig)
